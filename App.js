e0436b0fb8a5d9c12a6f48931e5002d57cea334b
Ritik Choudhary <96858347+ritik1576@users.noreply.github.com>
2022-12-10 19:52:05 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,61 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Textform from "./components/Textform";
+import React, { useState } from "react";
+import Alert from "./components/Alert";
+import About from "./components/About";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+function App() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  const showalert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  };
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#041631";
+      showalert("Dark mode has been enable", "succes");
+      // document.title = "Textutils - dark mode";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showalert("Light mode has been enable", "succes");
+      // document.title = "Textutils - light mode";
+    }
+  };
+  return (
+    <>
+    <Router>
+      <Navbar aboutText="About" mode={mode} toggleMode={toggleMode} />
+      {/* <Navbar/> */}
+      <Alert Alert={alert} />
+      <div className="container my-3">
+        <Routes>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Textform
+              showalert={showalert}
+              heading=" Enter the text to analyze"
+              mode={mode}
+            />
+          </Route>
+        </Routes>
+      </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
